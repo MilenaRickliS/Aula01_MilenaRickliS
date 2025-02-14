443ddca5e626d18720e908ffd0b468834b994036
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2025-02-13 21:13:57 -0300

    commit

---
+++ ex1.dart
@@ -0,0 +1,11 @@
+void main(){
+    int idade = 25;
+    double altura = 1.75;
+    String nome = "Pedro";
+    bool estuda = true;
+ 
+    print("Nome: $nome");
+    print("Idade: $idade");
+    print("Altura: $altura");
+    print("Estuda: $estuda");
+}
